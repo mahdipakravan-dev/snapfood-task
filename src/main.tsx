79c0ca7a1abfe8560aac08444cb40bceaa003ece
mahdipakravan-dev <mahdipakravan79@gmail.com>
2023-11-16 18:58:58 +0330

    feat : handle router

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import { Provider } from 'react-redux';
+import {Provider} from 'react-redux';
 import store from "./store";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {RestaurantPage} from "./pages/restaurant.tsx";
+
+const router = createBrowserRouter([
+    {
+        path : "/restaurants",
+        element : <RestaurantPage/>
+    }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Provider store={store}>
-        <App/>
-    </Provider>
+        <Provider store={store}>
+            <RouterProvider router={router}/>
+        </Provider>
   </React.StrictMode>,
 )
---
+++ src/pages/restaurant.tsx
@@ -0,0 +1,9 @@
+import {VendorsList} from "../components/vendors-list";
+
+export const RestaurantPage = () => {
+    return (
+        <div className={"container"}>
+            <VendorsList/>
+        </div>
+    );
+};
